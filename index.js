e78251625535c55f03be2fd3d78a4cee09abbe80
Joe McPartland <joemcpartland@gmail.com>
2021-09-29 07:57:21 -0600

    commit tree 5bb2e964404acbaa62fbc3dc64af86d5fb9e3284

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let multiply
+let num1 = 2
+let num2 = 31
+
+multiply = (num1 * num2)
+
+let random = Math.floor(Math.random() * 10)
+
+let num3 = 16
+let num4 = 6
+let mod = (num3 % num4)
+
+let max = (Math.max(3, 5, 20))
+
